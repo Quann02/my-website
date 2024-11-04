e084eff941aaba8ca2ac32a668157d93d1f6eb1f
Phu Quan <phuquan@Phus-MBP.hsd1.wa.comcast.net>
2024-11-04 12:54:15 -0800

    new change to NavBar Width

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from './Components/NavBar/NavBar.js';
 import Intro from './Components/Professional/Introduction/Intro.js';
+import Edu from './Components/Professional/Education/Edu.js';
 
 function App() {
   return (
     <div className='app'>
       <Navbar />
       <Intro />
+      <Edu />
     </div>
   );
 }
---
+++ src/Components/Professional/Education/Edu.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Edu.css'
+
+function Edu() {
+    return (
+        <div className='education'>
+          <div>
+            </div>
+          <h1>Greeting</h1>
+          <h2>Passionate in Cybersecurity and Information Technology</h2>
+        </div>
+    );
+  }
+  
+  export default Edu;
