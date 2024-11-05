de3f48d2709ef7b19b3a353c0fdc29377aebeeaa
Quann02 <phuquan@Phus-MBP.hsd1.wa.comcast.net>
2024-11-05 14:25:47 -0800

    new change to navbar.js and css, intro.js and css, and edu.js and css

--- src/Components/Professional/Education/Edu.js
+++ src/Components/Professional/Education/Edu.js
@@ -1,15 +1,23 @@
 import React from "react";
-import './Edu.css'
+import './Edu.css';
 
 function Edu() {
     return (
-        <div className='education'>
-          <div>
+        <div className='education' id='edu-section'>
+            <div className='edu-content'>
+                <h1>Education</h1>
+                <h2>University of Washington, Seattle, WA</h2>
+                <p>
+                    <strong>Bachelor of Science in Informatics (Cybersecurity)</strong> <br />
+                    Expected Graduation: December 2025 <br />
+                    GPA: 3.4
+                </p>
+                <p>
+                    <strong>Relevant Coursework:</strong> Information Security, Cybersecurity, Web Development, Data Programming, Data Management and Security, Network Defenses, Risk Assessment, Product Management
+                </p>
             </div>
-          <h1></h1>
-          <h2></h2>
         </div>
     );
-  }
-  
-  export default Edu;
+}
+
+export default Edu;
--- src/Components/Professional/Introduction/Intro.js
+++ src/Components/Professional/Introduction/Intro.js
@@ -3,11 +3,19 @@ import './Intro.css';
 
 function Intro() {
   return (
-      <div className='intro'>
-        <div>
-          </div>
-        <h1>Greeting, My name is Phu Quan</h1>
+    <div className="intro">
+      <div className="intro-content">
+        <h1>Greeting, Hope You are Doing Good</h1>
+        <h2>
+          I’m Phu Quan, a fourth-year student at the University of Washington, majoring in Informatics with a minor in Data Science. 
+          My background combines strong technical skills in web development, cybersecurity, and data analytics, 
+          with experience in Python, JavaScript, R, and Postgres. I’ve completed coursework and real-world 
+          projects, including a web API server project and a cybersecurity research project on firewalls at Carnegie Mellon University. 
+          Outside of academics, I bring hands-on experience as a Database Assistant and a consultative approach, 
+          aiming to drive value through both technical contributions and strategic thinking.
+        </h2>
       </div>
+    </div>
   );
 }
 
